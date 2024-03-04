0bb4d0223ca568df4fa336f271ccdbc6ba9e26ce
ryry6971 <ryanmusico12702@gmail.com>
2024-03-05 00:56:18 +0800

    2.0

--- resources/views/admin/pediatrics.blade.php
+++ resources/views/admin/pediatrics.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" /><link rel="icon" type="" href="{{ asset('images/logocircle.png') }}" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
    <title>The Queen's</title>
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}" />
