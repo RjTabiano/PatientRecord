ad91c75e5d5f8a727f7313c333e911ddfccf4365
RjTabiano <tabianorj@gmail.com>
2024-03-04 21:27:01 +0800

    update

---
+++ app/Http/Controllers/UserApiController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+class UserApiController extends Controller
+{
+    public function index(){
+        $users = User::all();
+
+        return response()->json([
+            'status' => 200,
+            'users'  => $users
+        ], 200);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        }else {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            if ($user){
+                return response()->json([
+                    'status' => 200,
+                    'message' => "User registered successfully"
+                ], 200);
+            }else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Something went wrong"
+                ], 500);
+            }
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('authToken')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/index', [UserApiController::class, 'index']);
+Route::post('/register', [UserApiController::class, 'register']);
+Route::post('/login', 'App\Http\Controllers\UserApiController@login');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
